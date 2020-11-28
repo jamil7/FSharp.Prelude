93fa5025d74d6711281b0de0f16e6271dd0d80f8
Jamil Maqdis Anton <jamil.maqdis-anton@insurello.se>
2020-11-28 18:42:15 +0100

    Refactor.

--- src/Result.fs
+++ src/Result.fs
@@ -3,17 +3,18 @@
 [<AutoOpen>]
 module ResultOperators =
     /// Infix map operator.
-    let inline (<!>) (f: 'a -> 'b) (result: Result<'a, 'c>): Result<'b, 'c> = Result.map f result
+    let inline (<!>) (f: 'a -> 'b) (result: Result<'a, 'e>): Result<'b, 'e> = Result.map f result
 
     /// Infix apply operator.
-    let inline (<*>) (f: Result<('a -> 'b), 'c>) (result: Result<'a, 'c>): Result<'b, 'c> =
+    let inline (<*>) (f: Result<('a -> 'b), 'e>) (result: Result<'a, 'e>): Result<'b, 'e> =
         match f, result with
         | Ok fOk, Ok resOk -> Ok(fOk resOk)
         | Error e, _ -> Error e
         | _, Error e -> Error e
 
     /// Infix bind operator.
-    let inline (>>=) (f: 'a -> Result<'b, 'c>) (result: Result<'a, 'c>): Result<'b, 'c> = Result.bind f result
+    let inline (>>=) (f: 'a -> Result<'b, 'e>) (result: Result<'a, 'e>): Result<'b, 'e> = Result.bind f result
+
 
 namespace FSharp.Prelude
 
@@ -22,34 +23,32 @@ open FSharp.Prelude.Operators.Result
 [<RequireQualifiedAccess>]
 module Result =
     /// Wraps a value in an Ok Result.
-    let singleton (value: 'a): Result<'a, 'b> = Ok value
+    let singleton (value: 'a): Result<'a, 'e> = Ok value
 
-    let apply (f: Result<('a -> 'b), 'c>) (result: Result<'a, 'c>): Result<'b, 'c> = f <*> result
+    let apply (f: Result<('a -> 'b), 'e>) (result: Result<'a, 'e>): Result<'b, 'e> = f <*> result
 
-    let bindError (f: 'a -> 'b) (result: Result<'c, 'a>): Result<'c, 'b> =
+    let bindError (f: 'e1 -> Result<'a, 'e2>) (result: Result<'a, 'e1>): Result<'a, 'e2> =
         match result with
         | Ok ok -> Ok ok
-        | Error error -> Error(f error)
+        | Error error -> f error
 
-    let map2 (f: 'a -> 'b -> 'c) (result1: Result<'a, 'd>) (result2: Result<'b, 'd>): Result<'c, 'd> =
+    let map2 (f: 'a -> 'b -> 'c) (result1: Result<'a, 'e>) (result2: Result<'b, 'e>): Result<'c, 'e> =
         f <!> result1 <*> result2
 
-    let andMap (result: Result<'a, 'b>) (f: Result<('a -> 'c), 'b>): Result<'c, 'b> = map2 (|>) result f
+    let andMap (result: Result<'a, 'e>) (f: Result<('a -> 'b), 'e>): Result<'b, 'e> = map2 (|>) result f
 
-    let andApply (result: Result<'a, 'b>) (f: Result<('a -> 'c), 'b>): Result<'c, 'b> = f <*> result
-
-    let sequence (results: Result<'a, 'b> list): Result<'a list, 'b> =
+    let sequence (results: Result<'a, 'e> list): Result<'a list, 'e> =
         List.foldBack (fun head tail -> (fun head tail -> head :: tail) <!> head <*> tail) results (singleton [])
 
-    let zip (result1: Result<'a, 'b>) (result2: Result<'c, 'b>): Result<('a * 'c), 'b> =
+    let zip (result1: Result<'a, 'e>) (result2: Result<'b, 'e>): Result<('a * 'b), 'e> =
         (fun a b -> a, b) <!> result1 <*> result2
 
-    let ofOption (error: 'a) (option: 'b option): Result<'b, 'a> =
+    let ofOption (error: 'e) (option: 'a option): Result<'a, 'e> =
         match option with
         | Some thing -> Ok thing
         | None -> Error error
 
-    let ofChoice (choice: Choice<'a, 'b>): Result<'a, 'b> =
+    let ofChoice (choice: Choice<'a, 'e>): Result<'a, 'e> =
         match choice with
         | Choice1Of2 left -> Ok left
         | Choice2Of2 right -> Error right
@@ -57,39 +56,42 @@ module Result =
 [<AutoOpen>]
 module ResultCE =
     type ResultBuilder() =
-        member _.Return(x) = Result.singleton x
+        member _.Return(value: 'a): Result<'a, 'e> = Result.singleton value
 
-        member _.ReturnFrom(m: Result<_, _>) = m
+        member _.ReturnFrom(result: Result<'a, 'e>): Result<'a, 'e> = result
 
-        member _.Zero() = Result.singleton ()
+        member _.Zero(): Result<unit, 'e> = Result.singleton ()
 
-        member _.Bind(m, f) = f >>= m
+        member _.Bind(result: Result<'a, 'e>, f: 'a -> Result<'b, 'e>): Result<'b, 'e> = f >>= result
 
-        member _.Bind((_, error), f) = Result.bindError f error
+        member _.Bind(error: Result<'a, 'e1>, f: 'e1 -> Result<'a, 'e2>): Result<'a, 'e2> = Result.bindError f error
 
-        member _.Delay(f: unit -> Result<_, _>) = f
+        member _.Delay(f: unit -> Result<'a, 'e>): unit -> Result<'a, 'e> = f
 
-        member _.Run(f: unit -> Result<_, _>) = f ()
+        member _.Run(f: unit -> Result<'a, 'e>): Result<'a, 'e> = f ()
 
-        member _.Combine(m: Result<unit, 'b>, f: unit -> Result<'a, 'b>) = f >>= m
+        member _.Combine(result: Result<unit, 'e>, f: unit -> Result<'a, 'e>): Result<'a, 'e> = f >>= result
 
-        member _.TryWith(f: unit -> Result<'a, 'b>, g: exn -> Result<'a, 'b>) =
+        member _.TryWith(f: unit -> Result<'a, 'e>, g: exn -> Result<'a, 'e>): Result<'a, 'e> =
             try
                 f ()
-            with e -> g e
+            with exn -> g exn
 
-        member _.TryFinally(f: unit -> Result<'a, 'b>, g: unit -> unit) =
+        member _.TryFinally(f: unit -> Result<'a, 'e>, g: unit -> unit): Result<'a, 'e> =
             try
                 f ()
             finally
                 g ()
 
-        member _.Using(m: 'a :> System.IDisposable, f: 'a -> Result<'a, 'b>) =
+        member _.Using(disposable: 'a :> System.IDisposable, f: 'a -> Result<'a, 'e>): Result<'a, 'e> =
             try
-                (fun () -> f m) ()
+                (fun () -> f disposable) ()
             finally
-                (fun () -> if not (obj.ReferenceEquals(m, null)) then m.Dispose()) ()
+                (fun () ->
+                    if not (obj.ReferenceEquals(disposable, null))
+                    then disposable.Dispose()) ()
 
-        member _.BindReturn(m, f) = f <!> m
+        member _.BindReturn(result: Result<'a, 'e>, f: 'a -> 'b): Result<'b, 'e> = f <!> result
 
-        member _.MergeSources(m1, m2) = Result.zip m1 m2
+        member _.MergeSources(result1: Result<'a, 'e>, result2: Result<'b, 'e>): Result<'a * 'b, 'e> =
+            Result.zip result1 result2
