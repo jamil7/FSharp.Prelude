3f6fe835760d66da4ab775a1c1e05c6fcdc88ee5
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-04-10 16:57:42 +0200

    Some list extensions for Async

---
+++ src/List.fs
@@ -0,0 +1,30 @@
+namespace FSharp.Prelude
+
+module List =
+
+    let cons head tail = head :: tail
+
+    [<AutoOpen>]
+    module AsyncOperation =
+
+        open FSharp.Prelude.Operators.Async
+
+        let traverseAsyncM (f: 'a -> Async<'b>) (asyncOps: 'a list) : Async<'b list> =
+            List.foldBack
+                (fun head tail ->
+                    f head
+                    >>= (fun h -> tail >>= (fun t -> (->>) (cons h t))))
+                asyncOps
+                ((->>) [])
+
+        let traverseAsyncA (f: 'a -> Async<'b>) (asyncOps: 'a list) : Async<'b list> =
+            List.foldBack (fun head tail -> cons <!> f head <*> tail) asyncOps ((->>) [])
+
+        let traverseAsyncAParallel (f: 'a -> Async<'b>) (asyncOps: 'a list) : Async<'b list> =
+            List.foldBack (fun head tail -> cons <!> f head <&> tail) asyncOps ((->>) [])
+
+        let sequenceAsyncM (asyncOps: Async<'a> list) : Async<'a list> = traverseAsyncM id asyncOps
+
+        let sequenceAsyncA (asyncOps: Async<'a> list) : Async<'a list> = traverseAsyncA id asyncOps
+
+        let sequenceAsyncAParallel (asyncOps: Async<'a> list) : Async<'a list> = traverseAsyncAParallel id asyncOps
