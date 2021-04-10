d77b1c9ed8b73ff8be431a76eeab45fe93f68272
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-04-10 20:52:06 +0200

    re add

---
+++ src/List.fs
@@ -0,0 +1,17 @@
+namespace FSharp.Prelude
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+[<RequireQualifiedAccess>]
+module List =
+
+    let cons head tail = head :: tail
+
+    // Options operations
+
+    let traverseOptionM (f: 'a -> Option<'b>) (options: 'a list) : Option<'b list> = Option.traverseM f options
+
+    let traverseOptionA (f: 'a -> Option<'b>) (options: 'a list) : Option<'b list> = Option.traverseA f options
+
+    let sequenceOptionM (options: Option<'a> list) : Option<'a list> = Option.sequenceM options
+
+    let sequenceOptionA (options: Option<'a> list) : Option<'a list> = Option.traverseA id options
