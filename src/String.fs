efe3223ae9c31613824c8e7915616c6f3e50392c
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-05-04 23:07:06 +0200

    reorder

--- src/String.fs
+++ src/String.fs
@@ -4,11 +4,18 @@ open System
 
 [<RequireQualifiedAccess>]
 module String =
+
+    let append str1 str2 : string = str1 + str2
+
     let contains (value: string) (source: string) : bool = source.Contains value
 
     let isEmpty (str: string) : bool = str = ""
 
-    let reverse (str: string) : string = String(str.ToCharArray() |> Array.rev)
+    let join (separator: string) (values: string list) : string = String.Join(separator, values)
+
+    let padLeft (totalWidth: int) (str: string) = str.PadLeft totalWidth
+
+    let padRight (totalWidth: int) (str: string) = str.PadRight totalWidth
 
     let replace (oldValue: string) (newValue: string) (source: string) =
         if String.length oldValue = 0 then
@@ -16,26 +23,20 @@ module String =
         else
             source.Replace(oldValue, newValue)
 
-    let append str1 str2 : string = str1 + str2
+    let reverse (str: string) : string = String(str.ToCharArray() |> Array.rev)
 
     let split (separator: string) (str: string) = str.Split(separator) |> List.ofArray
 
-    let join (separator: string) (values: string list) : string = String.Join(separator, values)
-
     let lines (str: string) : string list = split Environment.NewLine str
 
-    let toUpper (str: string) : string = str.ToUpper()
+    let startsWith (value: string) (source: string) : bool = source.StartsWith value
 
     let toLower (str: string) : string = str.ToLower()
 
-    let padLeft (totalWidth: int) (str: string) = str.PadLeft totalWidth
-
-    let padRight (totalWidth: int) (str: string) = str.PadRight totalWidth
+    let toUpper (str: string) : string = str.ToUpper()
 
     let trim (str: string) : string = str.Trim()
 
-    let trimStart (str: string) : string = str.TrimStart()
-
     let trimRight (str: string) : string = str.TrimEnd()
 
-    let startsWith (value: string) (source: string) : bool = source.StartsWith value
+    let trimStart (str: string) : string = str.TrimStart()
