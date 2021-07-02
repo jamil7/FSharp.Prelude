51a28c1665cc160b7a6a8674b3e0ae2cb5a17cfe
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2021-07-02 23:00:17 +0200

    add Prelude.fs

---
+++ src/Prelude.fs
@@ -0,0 +1,25 @@
+namespace Prelude
+
+[<AutoOpen>]
+module Prelude =
+    /// Flips the order of the arguments.
+    let inline flip f a b = f b a
+
+    /// Returns the first argument always.
+    let inline always a _ = a
+
+    /// Place holder for unimplemented code.
+    let todo<'a> : 'a =
+        "Not implemented"
+        |> System.NotImplementedException
+        |> raise
+
+    /// Applies `f` to `a` and returns `a`. 
+    let inline tap f a =
+        f a
+        a
+    
+    
+
+    
+    
