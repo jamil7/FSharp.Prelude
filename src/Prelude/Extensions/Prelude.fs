12107cfe16ebf43edcd5f2fcc2c67059d46b2f30
Jamil Maqdis Anton <jamilmaqdis@gmail.com>
2022-01-14 00:34:23 +0100

    Move Tasks to its own project (#23)

--- src/Prelude/Extensions/Prelude.fs
+++ src/Prelude/Extensions/Prelude.fs
@@ -3,10 +3,10 @@ namespace Prelude
 [<AutoOpen>]
 module FunctionalExtensions =
     /// Flips the order of the arguments.
-    let inline flip f a b = f b a
+    let inline flip (f: 'a -> 'b -> 'c) (a: 'b) (b: 'a) : 'c = f b a
 
     /// Returns the first argument always.
-    let inline always a _ = a
+    let inline always (a: 'a) (_: 'b) : 'a = a
 
     /// Place holder for unimplemented code.
     let todo<'a> : 'a =
@@ -15,6 +15,6 @@ module FunctionalExtensions =
         |> raise
 
     /// Applies `f` to `a` and returns `a`.
-    let inline tap f a =
+    let inline tap (f: 'a -> unit) (a: 'a) : 'a =
         f a
         a
